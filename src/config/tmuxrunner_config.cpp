1339cb7bd1a4f93edaadde8ac45f0e6ea108378b
alex1701c <alex1701c.dev@gmx.net>
2022-02-06 12:25:39 +0100

    Do not define unneeded and deprecated KCM keyword

--- src/config/tmuxrunner_config.cpp
+++ src/config/tmuxrunner_config.cpp
@@ -7,7 +7,7 @@
 #include <QtCore/QDir>
 #include <KShell>
 
-K_PLUGIN_FACTORY(TmuxRunnerConfigFactory, registerPlugin<TmuxRunnerConfig>("kcm_krunner_tmuxrunner");)
+K_PLUGIN_FACTORY(TmuxRunnerConfigFactory, registerPlugin<TmuxRunnerConfig>();)
 
 TmuxRunnerConfigForm::TmuxRunnerConfigForm(QWidget *parent) : QWidget(parent) {
     setupUi(this);
